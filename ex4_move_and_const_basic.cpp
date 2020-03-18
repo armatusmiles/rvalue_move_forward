9be1fe180c3538b665877f2fd39574aa038cab93
Anatolii Kurotych <akurotych@gmail.com>
2020-03-18 11:49:11 +0200

    update ex4

--- ex4_move_and_const_basic.cpp
+++ ex4_move_and_const_basic.cpp
@@ -14,9 +14,12 @@ void foo(A a)
 void foo2(A&& a)
 {}
 
+template<typename T>
+void foo3(T&&) {} // Take universal reference(!)
+
 int main()
 {
-    A a1, a2, a3, a4, a5, a6;
+    A a1, a2, a3, a4, a5, a6, a7;
     const A ac;
     cout << "\n";
 
@@ -32,4 +35,7 @@ int main()
 
     foo2(static_cast<A&&>(a5)); // pass object by rvalue reference. (Constructors don't not called)
     foo2(std::move(a6)); // pass object by rvalue reference. (Constructors don't not called)
+    
+    foo3(a7); // pass object by lvalue reference
+    foo3(std::move(a7)); // pass object by rvalue reference
 }
